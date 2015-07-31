33fdadb05fbc185f4c0258eb8072ee8813ffe614
Martin Thielecke <github@mthie.com>
2015-07-31 23:53:25 +0000

    [gobuild] initial hook to check the build

---
+++ githook-gobuild/hook.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/mthie/git-gohooks/general"
+)
+
+func main() {
+	files := general.GetChangedGoFiles()
+	if files == nil {
+		os.Exit(0)
+		return
+	}
+
+	os.Chdir(general.GetGitRoot())
+
+	_, status := general.RunCommand("go", "build", ".", "./...")
+	if status != 0 {
+		fmt.Fprint(os.Stderr, "Build failed, please commit only stuff that builds.\n")
+		os.Exit(1)
+		return
+	}
+	general.RunCommand("go", "clean", ".", "./...")
+}
