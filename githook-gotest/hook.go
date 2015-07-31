d8181277e677a206f57e4dc609101ff1fb516e82
Martin Thielecke <github@mthie.com>
2015-07-31 23:58:20 +0000

    [gotest] new hook for go test and added binary to gitignore

---
+++ githook-gotest/hook.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/mthie/git-gohooks/general"
+)
+
+func main() {
+	files := general.GetChangedGoFiles()
+	if files == nil {
+		os.Exit(0)
+		return
+	}
+
+	os.Chdir(general.GetGitRoot())
+
+	_, status := general.RunCommand("go", "test", "-test.short", "./...")
+	if status != 0 {
+		fmt.Fprint(os.Stderr, "Test failed, please commit only stuff that works.\n")
+		os.Exit(1)
+		return
+	}
+}
